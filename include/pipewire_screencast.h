d54702f717b877ca32f1df38a94b0aeaf26ef5b3
columbarius <co1umbarius@protonmail.com>
2021-06-17 23:36:45 +0200

    screencast: introduce xdpw_pwr_screencopy_dmabuf_frame

--- include/pipewire_screencast.h
+++ include/pipewire_screencast.h
@@ -14,6 +14,11 @@ struct xdpw_pwr_screencopy_frame {
 	struct wl_buffer *buffer;
 };
 
+struct xdpw_pwr_screencopy_dmabuf_frame {
+	struct wl_buffer *buffer;
+	struct gbm_bo *bo;
+};
+
 void xdpw_pwr_import_buffer(struct xdpw_screencast_instance *cast);
 void xdpw_pwr_export_buffer(struct xdpw_screencast_instance *cast);
 void pwr_update_stream_param(struct xdpw_screencast_instance *cast);
